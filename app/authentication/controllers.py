41d3b972d23d40443f798c5d222bc625f3f30c57
Christian Renato Benavides Castillo <chanochambure@bitbucket.org>
2015-10-13 11:36:59 -0500

    #12 - Uso de Expiración del Token

--- app/authentication/controllers.py
+++ app/authentication/controllers.py
@@ -30,6 +30,7 @@ def profile():
 
     user = User.verify_token(session['token'])
     if user is None:
+        flash(u'Token Time Out', 'error')
         return redirect(url_for('auth.signin'))
     else:
         return render_template('authentication/profile.html')
