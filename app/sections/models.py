574af97535b64f901029ac4598d7f631e42a9f45
Christian Renato Benavides Castillo <chanochambure@bitbucket.org>
2015-10-14 23:05:56 -0500

    Merge - Modelado de Secciones - Desarr2

---
+++ app/sections/models.py
@@ -0,0 +1,21 @@
+from app import db
+from flask import redirect, current_app
+
+
+class Sections(db.Model):
+
+    __tablename__ = 'cms_sections'
+
+    id       = db.Column('id', db.Integer, primary_key=True, \
+                autoincrement='ignore_fk')
+    section_name = db.Column(db.String(128),  nullable=False, unique=True)
+    description = db.Column(db.String(128),  nullable=True)
+
+    def __init__(self, section_, description_):
+
+        self.section_name = section_.title()
+        self.description = description_
+    
+
+    def __repr__(self):
+        return '<Sections %r>' % (self.section_name)
