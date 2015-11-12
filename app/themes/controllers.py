eea2f737c15999ac9bd54a91f3c69543802bc2f4
Vito Alejandro Huanqui Escobedo <vito.huanqui@ucsp.edu.pe>
2015-11-12 18:41:04 -0500

    #45 avance de Configurar PLantillas

---
+++ app/themes/controllers.py
@@ -0,0 +1,65 @@
+from flask import (
+    Blueprint,
+    request,
+    render_template,
+    flash,
+    g,
+    session,
+    redirect,
+    url_for
+)
+from app import db
+from app.authentication.constants import ReadRole, CommentRole, WriteRole
+from app.authentication.models import User
+from app.themes.forms import ConfigurationForm
+from app.themes.models import Theme
+
+mod_theme = Blueprint('themes', __name__, url_prefix='/themes')
+
+
+@mod_theme.route('/config', methods=['GET', 'POST'])
+def configuration_theme():
+    if 'token' not in session:
+        return redirect(url_for('auth.signin'))
+    user = User.verify_token(session['token'])
+    if user is None:
+        flash(u'Token Time Out', 'error')
+        return redirect(url_for('auth.signin'))
+    if not user.role>>2:
+        #return acces denied
+        abort(401)
+    form = ConfigurationForm()
+    form.user_name.data = user.username
+    if request.method == 'POST':
+        if form.validate():
+        #falta mejorar el logo 
+            theme = Theme(name=form.name.data,
+                          default_use=form.default_use.data,
+                          title=form.title.data,
+                          #
+                          logo=form.logo.data,
+                          #
+                          resources='themes/'+name,
+                          description=form.description.data,
+                          username=form.user_name.data)
+            db.session.add(theme)
+            db.session.commit()            
+            return redirect(url_for('auth.profile'))
+    return render_template("themes/configuration.html", form=form)
+
+@mod_auth.route('/signin/', methods=['GET', 'POST'])
+def signin():
+    form = LoginForm(request.form)
+    if 'token' in session:
+        user = User.verify_token(session['token'])
+        if user:
+            return redirect(url_for('auth.profile'))
+    if request.method == 'POST':
+        if form.validate():
+            user = User.query.filter_by(email=form.email.data).first()
+            session['user_id'] = user.id
+            session['token'] = user.generate_token()
+            session['email'] = user.email
+            session['user_name'] = user.username
+            return redirect(url_for('auth.profile'))
+    return render_template("authentication/signin.html", form=form)
---
+++ app/themes/forms.py
@@ -0,0 +1,25 @@
+from flask.ext.wtf import Form # , RecaptchaField
+from wtforms import TextField, SubmitField  # BooleanField
+from wtforms import TextAreaField, HiddenField, FileField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from wtforms.validators import EqualTo, Length
+from models import db, Theme
+
+
+class ConfigurationTheme(Form):
+	name           = TextField('theme', [Required("Please enter a theme")],
+                        filters=[strip_filter])
+	default_use    = RadioField('Default?',
+			       			 [validators.Required()],
+			        		 choices=[('yes', 'Default'), ('no', 'Not default')], default='yes'
+			                )
+    title          = TextField('Title', [Required("Please enter a title")],
+                        filters=[strip_filter])
+    resources      = HiddenField()
+    logo           = FileField()
+    description    = TextAreaField('description',[Required("Please enter a description")],
+                        filters=[strip_filter])
+    user_name      = HiddenField()
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
