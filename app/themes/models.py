0d91b843439abd3eb86b572fff2ede9423244f4b
Oscar Chujutalli <oscarchs@bitbucket.com>
2015-11-12 23:04:31 -0500

    agregado field logo a modelo de plantilla

--- app/themes/models.py
+++ app/themes/models.py
@@ -8,16 +8,17 @@ class Theme(db.Model):
     name          = db.Column(db.String(100))
     default_use   = db.Column(db.Boolean)
     title         = db.Column(db.String(100))
+    logo          = db.Column(db.String(100))
     resources     = db.Column(db.String(100))
     description   = db.Column(db.Text)
     user_name     = db.Column(db.String(10), db.ForeignKey(User.username))
 
 
-    def __init__(self, name, default_use, title,resources,description,user_name):
-
+    def __init__(self, name, default_use, title,logo,resources,description,user_name):
         self.name           = name
         self.deafult_use    = default_use
         self.title          = title
+        self.logo           = logo
         self.resources      = resources
         self.description    = description
         self.user_name      = user_name
